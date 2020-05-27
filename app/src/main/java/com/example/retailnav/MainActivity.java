b58daee11b68a088626e999e4e39b34ec4c664cb
UmaAthikala <54384809+UmaAthikala@users.noreply.github.com>
2020-05-27 10:59:19 -0400

    functionality addition: get the checked items, map them to item category, display the selected item categories only in the map_activity.

--- app/src/main/java/com/example/retailnav/MainActivity.java
+++ app/src/main/java/com/example/retailnav/MainActivity.java
@@ -1,34 +1,74 @@
 package com.example.retailnav;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> shoppinglist=new ArrayList<String>();
     private Button HelpMeShop_button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        HelpMeShop_button=(Button) findViewById(R.id.HelpMeShop);
+        HelpMeShop_button= findViewById(R.id.HelpMeShop);
         HelpMeShop_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                displayshoppingList();
                 showMapActivity();
             }
         });
     }
 
+    public void displayshoppingList()
+    {
+        /*Iterator<String> shopListItr = shoppinglist.iterator();
+        while (shopListItr.hasNext()) {
+            System.out.println("\n.......................... "+shopListItr.next()+" ...........................................");
+        }*/
+        Set<String> hashSet = new LinkedHashSet(shoppinglist);
+        shoppinglist = new ArrayList<String>(hashSet);
+
+       /* shopListItr = shoppinglist.iterator();
+        while (shopListItr.hasNext()) {
+            System.out.println("\n.......................... "+shopListItr.next()+" ...........................................");
+        }*/
+    }
+    public void modifyShoppingList(View v)
+    {
+        CheckBox checkBox = (CheckBox)v;
+        String idString = v.getResources().getResourceEntryName(v.getId());
+        idString = idString.replaceAll("[0123456789]","");
+        if(checkBox.isChecked())
+        {
+            if(!shoppinglist.contains(idString))
+            {
+                shoppinglist.add(idString);
+            }
+        }
+        else
+        {
+            if(shoppinglist.contains(idString))
+            {
+                shoppinglist.remove(idString);
+            }
+        }
+    }
+
     public void showMapActivity()
     {
         Intent intent_activity_map = new Intent(this, activity_map.class);
+        intent_activity_map.putExtra("shoppinglist", shoppinglist);
         startActivity(intent_activity_map);
-
     }
 }
--- app/src/main/java/com/example/retailnav/activity_map.java
+++ app/src/main/java/com/example/retailnav/activity_map.java
@@ -1,31 +1,27 @@
 package com.example.retailnav;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-
-import android.content.res.AssetManager;
+import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class activity_map extends AppCompatActivity {
-
+    ArrayList<String> shoppingList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Intent i = getIntent();
+        shoppingList = (ArrayList<String>) i.getSerializableExtra("shoppinglist");
         // To get names of all files inside the "Files" folder
         BufferedReader reader = null;
         try {
@@ -34,7 +30,7 @@ public class activity_map extends AppCompatActivity {
             // do reading, usually loop until end of file reading
             String mLine;
             while ((mLine = reader.readLine()) != null) {
-                System.out.println("\n*****\n*******\n*****\n****\n"+mLine);
+                //System.out.println("\n*****\n*******\n*****\n****\n"+mLine);
                 String node_details[]=mLine.split(",");
                 create_nodes(node_details);
             }
@@ -54,28 +50,30 @@ public class activity_map extends AppCompatActivity {
 
     protected void create_nodes(String node_details[])
     {
-        int node_id=Integer.parseInt(node_details[0]);
-        int node_x=Integer.parseInt(node_details[2]);
-        int node_y=Integer.parseInt(node_details[3]);
-        System.out.println(node_id+","+node_details[1]+","+node_x+","+node_y);
-        RelativeLayout map_layout;
-        map_layout = (RelativeLayout) findViewById(R.id.activity_map);
-        TextView node= new TextView(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        params.setMargins(node_x*20,0,0,node_y*20);
-        node.setLayoutParams(params);
-        node.setBackgroundColor(Color.YELLOW);
-        node.setPadding(2,2,2,2);
-        node.setId(node_id);
-        node.setText(node_details[1]);
-        node.setHeight(80);
-        node.setWidth(200);
+        if(shoppingList.contains(node_details[1])||node_details[1].contentEquals("Entrance")) {
+            int node_id = Integer.parseInt(node_details[0]);
+            int node_x = Integer.parseInt(node_details[2]);
+            int node_y = Integer.parseInt(node_details[3]);
+            //System.out.println(node_id + "," + node_details[1] + "," + node_x + "," + node_y);
+            RelativeLayout map_layout;
+            map_layout = (RelativeLayout) findViewById(R.id.activity_map);
+            TextView node = new TextView(this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+            params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            params.setMargins(node_x * 20, 0, 0, node_y * 20);
+            node.setLayoutParams(params);
+            node.setBackgroundColor(Color.YELLOW);
+            node.setPadding(2, 2, 2, 2);
+            node.setId(node_id);
+            node.setText(node_details[1]);
+            node.setHeight(80);
+            node.setWidth(200);
 
-        if (map_layout != null){
-            map_layout.addView(node);
+            if (map_layout != null) {
+                map_layout.addView(node);
+            }
         }
     }
 }
